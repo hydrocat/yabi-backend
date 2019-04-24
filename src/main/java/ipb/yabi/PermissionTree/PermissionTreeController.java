f3102cc46058d0618620a28288017e0e568e8926
hydrocat <vdrbandeiras@gmail.com>
2019-04-24 18:10:35 +0100

    DELETE /permission/{permissionID} now return a list with the deleted permissions's ids
    This is done so angular can remove the items in the list withou re-querying the backend

--- src/main/java/ipb/yabi/PermissionTree/PermissionTreeController.java
+++ src/main/java/ipb/yabi/PermissionTree/PermissionTreeController.java
@@ -3,23 +3,13 @@ package ipb.yabi.PermissionTree;
 import ipb.yabi.YabiUser.YabiUser;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.rest.webmvc.BasePathAwareController;
-import org.springframework.data.rest.webmvc.PersistentEntityResource;
-import org.springframework.data.rest.webmvc.PersistentEntityResourceAssembler;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
-import org.springframework.data.web.PagedResourcesAssembler;
-import org.springframework.hateoas.PagedResources;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -34,13 +24,13 @@ public class PermissionTreeController {
     PermissionRepository pr;
 
     @DeleteMapping("/permission/{node}")
-    public Integer deleteNode(@PathVariable PermissionTree node) {
+    public List<Long> deleteNode(@PathVariable PermissionTree node) {
         List<PermissionTree> permissions = pr.findAllBynodePathStartingWith(node.getNodePath());
-        Integer elementCount = permissions.size();
-
+        
+        List<Long> deletedIds = permissions.stream().map( p -> p.getId()).collect( Collectors.toList() );
         permissions.forEach(pr::delete);
 
-        return elementCount;
+        return deletedIds;
     }
 
     @GetMapping(path = "/permissions")
