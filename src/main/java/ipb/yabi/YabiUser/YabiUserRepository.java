34c3bb79690501d35baf3b9391045860c54b6fa8
hydrocat <vdrbandeiras@gmail.com>
2019-04-24 18:09:44 +0100

    Add @CrossOrigin to YabiUserRepository

--- src/main/java/ipb/yabi/YabiUser/YabiUserRepository.java
+++ src/main/java/ipb/yabi/YabiUser/YabiUserRepository.java
@@ -1,7 +1,9 @@
 package ipb.yabi.YabiUser;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface YabiUserRepository extends PagingAndSortingRepository<YabiUser, Long> {
     public YabiUser findByName(String name);
     
