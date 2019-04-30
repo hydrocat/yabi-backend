cebbd4a0d9ea1374113a22ef0572f3e34b0dcf7b
hydrocat <vdrbandeiras@gmail.com>
2019-04-30 19:03:41 +0100

    Add role-based authorization to all routes.

--- src/main/java/ipb/yabi/Configuration/SecurityConfiguration.java
+++ src/main/java/ipb/yabi/Configuration/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -39,6 +40,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/user").permitAll()
+                // Spring Repositories
+                .antMatchers("/directories/**", "/yabiUsers/**", "/permissionTrees/**", "/sqlQueries/**").hasRole("ADMIN")
+                // Custom Controllers
+                .antMatchers(HttpMethod.DELETE, "/permission/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .logout().permitAll();
